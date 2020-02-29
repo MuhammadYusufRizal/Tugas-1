13a6e9069d9a37490f03e308da8cd68c2d0f459c
HK <45817996+MuhammadYusufRizal@users.noreply.github.com>
2020-02-29 20:00:38 +0700

    Update main.java

--- Tugas-1/main.java
+++ Tugas-1/main.java
@@ -4,5 +4,6 @@ public class Latihan1{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Halo Semua");
+        System.out.println("Sampai Jumpa");
     }
 }
